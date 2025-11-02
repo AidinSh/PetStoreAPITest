cd93e4fae6888da1c7f3534cc00a3d8549666e7d
aidin.shahmoradi <aidin.shahmoradi@divar.ir>
2025-11-02 18:36:11 +0100

    Add project files.

---
+++ PetStoreAPITest/Models/Pet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetStoreAPITest.Models
+{
+    public class Pet
+    {
+        public long Id { get; set; }
+        public Category Category { get; set; } = new Category();
+        public string Name { get; set; } = string.Empty;
+        public List<string> PhotoUrls { get; set; } = new List<string>();
+        public List<Tag> Tags { get; set; } = new List<Tag>();
+        public string Status { get; set; } = string.Empty; 
+
+    }
+}
